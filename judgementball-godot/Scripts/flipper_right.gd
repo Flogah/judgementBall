91721503fc59cb5fcd4fa04b88918e6a38c3e997
BunterInferno <leafliehmann18@gmail.com>
2026-07-31 17:42:04 +0200

    initial commit

---
+++ judgementball-godot/Scripts/flipper_right.gd
@@ -0,0 +1,15 @@
+extends CharacterBody3D
+
+var curRot: float
+
+func _ready() -> void:
+	curRot = rotation_degrees.y
+	print(curRot)
+
+
+func _physics_process(delta: float) -> void:
+	if Input.is_action_pressed("rightClick"):
+		if rotation_degrees.y > -80: 
+			rotate(Vector3.UP, -10*delta)
+	elif rotation_degrees.y <= curRot:
+		rotate(Vector3.UP, 10*delta)
