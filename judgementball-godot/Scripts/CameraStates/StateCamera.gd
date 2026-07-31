e2d91c787cc3297deb0803bc43a8250ab7b8d59e
Lea Fliehmann <107272382+BunterInferno@users.noreply.github.com>
2026-07-31 23:59:32 +0200

    added camera smoothing
    dauer mit exportierter variable einstellbar

--- judgementball-godot/Scripts/CameraStates/StateCamera.gd
+++ judgementball-godot/Scripts/CameraStates/StateCamera.gd
@@ -5,10 +5,28 @@ class_name StateCamera
 @export var FlipCam: Camera3D
 @export var TelCam: Camera3D
 @export var TableCam: Camera3D
+@export var SwitchCam : Camera3D
+
+@export var transition_duration: float = 2.0
 
 func _ready() -> void:
 	freeCam = get_viewport().get_camera_3d()
-	print(freeCam)
 
 func changeCamera(newCam: Camera3D):
-	newCam.make_current()
+	var curCam = get_viewport().get_camera_3d()
+	SwitchCam.global_position = curCam.global_position
+	SwitchCam.global_rotation = curCam.global_rotation
+	SwitchCam.make_current()
+	
+	var tween = create_tween().set_parallel(true)
+	tween.set_trans(Tween.TRANS_CUBIC) 
+	tween.set_ease(Tween.EASE_IN_OUT)
+
+	tween.tween_property(
+		SwitchCam, 
+		"global_transform", 
+		newCam.global_transform, 
+		transition_duration
+	)
+
+	tween.finished.connect(func(): newCam.make_current())
