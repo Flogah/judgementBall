aef11c51ad120c3bf41df5a31f46987d985adf73
Mohito <89162350+Flogah@users.noreply.github.com>
2026-08-02 13:35:35 +0200

    created a simple room for the final stage

--- judgementball-godot/Scripts/papier.gd
+++ judgementball-godot/Scripts/papier.gd
@@ -1,14 +1,14 @@
 extends Interactable
 
-@onready var mesh_instance_3d: MeshInstance3D = %MeshInstance3D
-@onready var spot_light_3d: SpotLight3D = %SpotLight3D
+#@onready var mesh_instance_3d: MeshInstance3D = %MeshInstance3D
+#@onready var spot_light_3d: SpotLight3D = %SpotLight3D
 @onready var collisionShp: CollisionShape3D = $CollisionShape3D
 @export var clueNbr : int = 1
 
 var active:bool = false:
 	set(new_value):
 		active = new_value
-		spot_light_3d.visible = new_value
+		#spot_light_3d.visible = new_value
 
 func interact():
 	active = not active
