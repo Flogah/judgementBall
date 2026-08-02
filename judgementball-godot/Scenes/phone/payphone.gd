b9d81c338370177d23d47cc4f90c31ce4f7eb578
BunterInferno <leafliehmann18@gmail.com>
2026-08-02 12:37:24 +0200

    add phone and interactive rotation

---
+++ judgementball-godot/Scenes/phone/payphone.gd
@@ -0,0 +1,40 @@
+extends Interactable
+
+
+@onready var handle: MeshInstance3D = $Handle
+@onready var handlePosActive: Node3D = $handlePosActive
+@onready var handlePosPassive: Node3D = $handlePosPassive
+@export var transition_duration: float = 2.0
+var tween : Tween
+
+var phoneActive: bool = false
+	
+func interact():
+	if !phoneActive:
+		if tween:
+			tween.kill()
+		tween = create_tween().set_parallel(true)
+		tween.set_trans(Tween.TRANS_CUBIC) 
+		tween.set_ease(Tween.EASE_IN_OUT)
+
+		tween.tween_property(
+			handle, 
+			"global_transform", 
+			handlePosActive.global_transform, 
+			transition_duration
+		)
+		phoneActive = true
+	else:
+		if tween:
+			tween.kill()
+		tween = create_tween().set_parallel(true)
+		tween.set_trans(Tween.TRANS_CUBIC) 
+		tween.set_ease(Tween.EASE_IN_OUT)
+
+		tween.tween_property(
+			handle, 
+			"global_transform", 
+			handlePosPassive.global_transform, 
+			transition_duration
+			)
+		phoneActive = false
